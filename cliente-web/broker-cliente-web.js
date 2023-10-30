e9fe318cfcb7cd884855b64f671f2f7026697b26
Tobias <>
2023-10-30 15:11:49 -0300

    el broker-bodys medio que queda deprecado por el curls-test que permite generar request a el broker-mock y testear asi la interaccion con el cliente web. broker cliente web recibe bien los 2 poll y tambien se suscribe bien, falta probar el publish. el mock sufrio modificaciones necesarias para q funcione bien

--- cliente-web/broker-cliente-web.js
+++ cliente-web/broker-cliente-web.js
@@ -1,12 +1,95 @@
 const http = require('http');
 
+const ip = "localhost";
+const port = 3000
 const pollingIntervalAscensor = 1000;
 const pollingIntervalCambioEstado = 1000;
 
 
 
 function suscribirAscensor(ip, port) {
-  let topicPath = '/api/suscribe/ascensor'
+  let topicPath = '/api/ascensores/subscribe'
+
+  return new Promise((resolve, reject) => {
+    const options = {
+      method: 'POST',
+      hostname: ip,
+      port: port,
+      path: topicPath,
+    };
+
+    const req = http.request(options, (res) => {
+      let data = '';
+
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      res.on('end', () => {
+        if (res.statusCode === 200) {
+          try {
+            const jsonData = JSON.parse(data.toString());
+            console.log(topicPath + jsonData);
+            resolve(jsonData.id);
+          } catch (error) {
+            console.error('Error al parsear el body: ', error);
+            reject('Error al parsear el body');
+          }
+        } else {
+          reject(`Response no exitosa: ${res.statusCode}`);
+        }
+      });
+    });
+
+    req.on('error', (error) => {
+      console.error('Error en la request: ', error);
+      reject('Error en la request');
+    });
+
+    req.end();
+  })
+}
+
+
+function pollAscensor(ip, port, idAscensor) {
+  const topicPath = `/api/ascensores/poll?id=${idAscensor}`;
+  const options = {
+    method: 'GET',
+    hostname: ip,
+    port: port,
+    path: topicPath,
+  };
+
+  const req = http.request(options, (res) => {
+    let data = '';
+
+    res.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    res.on('end', () => {
+      if (res.statusCode === 200) {
+        try {
+          const jsonData = JSON.parse(data);
+          console.log(topicPath + jsonData);
+        } catch (error) {
+          console.error('Error al parsear el body: ', error);
+        }
+      } else if (res.statusCode === 204) {
+        console.log('Empty cache Ascensores', res.statusCode);
+      }
+      else {
+        console.log('Error polling number: ', res.statusCode);
+      }
+    });
+
+  });
+
+  req.end();
+}
+
+function suscribirCambioEstado(ip, port) {
+  const topicPath = '/api/cambio/subscribe';
 
   return new Promise((resolve, reject) => {
     const options = {
@@ -28,91 +111,8 @@ function suscribirAscensor(ip, port) {
           try {
             const jsonData = JSON.parse(data);
             console.log(topicPath + jsonData);
-            var idAscensor = "idAscensorDummy"
-            resolve(jsonData);
-          } catch (e) {
-            console.error('Error al parsear el body: ', error);
-            reject('Error al parsear el body');
-          }
-        } else {
-          reject(`Response no exitosa: ${res.statusCode}`);
-        }
-      });
-    });
-
-    req.on('error', (error) => {
-      console.error('Error en la request: ', error);
-      reject('Error en la request');
-    });
-
-    req.end();
-  })
-}
-
-
-function pollAscensor(ip, port) {
-  const topicPath = `/api/ascensores/poll?id=${idAscensor}`;
-  const options = {
-    method: 'GET',
-    hostname: ip,
-    port: port,
-    path: topicPath,
-  };
-
-  const req = http.request(options, (res) => {
-    let data = '';
-
-    res.on('data', (chunk) => {
-      data += chunk; 
-    });
-    
-    res.on('end', () => {
-      if (res.statusCode === 200) {
-        try {
-          const jsonData = JSON.parse(data);
-          console.log(topicPath + jsonData);
-        } catch (e) {
-          console.error('Error al parsear el body: ', error);
-        }
-      } else if (res.statusCode === 204) {
-        console.log('Empty cache Ascensores', res.statusCode);
-      }
-      else {
-        console.log('Error polling number: ', res.statusCode);
-      }
-    });
-  
-  });
-
-  req.end();
-}
-
-function suscribirCambioEstado(ip,port) {
-  const topicPath = '/api/cambio/subscribe';
-  
-  return new Promise((resolve, reject) => {
-    const options = {
-      method: 'POST',
-      hostname: ip,
-      port: port,
-      path: topicPath,
-    };
-
-    const req = http.request(options, (res) => {
-      let data = '';
-
-      res.on('data', (chunk) => {
-        data += chunk;          
-      });
-
-      res.on('end', () => {
-        if (res.statusCode === 200) {
-          try {
-            const jsonData = JSON.parse(data);
-            console.log(topicPath + jsonData);
-            var idCambio = "idCambioDummy"
-            resolve(jsonData);
-          } catch (e) {
+            resolve(jsonData.id);
+          } catch (error) {
             console.error('Error al parsear el body: ', error);
             reject('Error al parsear el body');
           }
@@ -127,9 +127,9 @@ function suscribirCambioEstado(ip,port) {
 }
 
 
-function pollCambioEstado(ip,port) {
+function pollCambioEstado(ip, port, idCambio) {
   const topicPath = `/api/cambio/poll?id=${idCambio}`;
-  
+
   const options = {
     method: 'GET',
     hostname: ip,
@@ -143,13 +143,13 @@ function pollCambioEstado(ip,port) {
     res.on('data', (chunk) => {
       data += chunk; //almacena la respuesta del servidor 
     });
-    
+
     res.on('end', () => {
       if (res.statusCode === 200) {
         try {
           const jsonData = JSON.parse(data);
-          console.log(topicPath + jsonData);
-        } catch (e) {
+          console.log(topicPath + jsonData.toString);
+        } catch (error) {
           console.error('Error al parsear el body: ', error);
         }
       } else if (res.statusCode === 204) {
@@ -159,7 +159,7 @@ function pollCambioEstado(ip,port) {
         console.log('Error polling number: ', res.statusCode);
       }
     });
-  
+
   });
 
   req.end();
@@ -167,7 +167,7 @@ function pollCambioEstado(ip,port) {
 
 function publicarCambioAscensor(ip, port) {
   topicPath = '/api/cambio/ascensor'
-  
+
   const postData = JSON.stringify({
     idAscensor: 1,
     estado: "ocupado",
@@ -190,7 +190,7 @@ function publicarCambioAscensor(ip, port) {
     let data = '';
 
     res.on('data', (chunk) => {
-      data += chunk; 
+      data += chunk;
     });
 
     res.on('end', () => {
@@ -207,12 +207,12 @@ function publicarCambioAscensor(ip, port) {
 }
 
 //Suscripcion doble
-suscribirAscensor().then(() => {
-  suscribirCambioEstado().then(() => {
-    setInterval(pollAscensor, pollingIntervalAscensor);
-    setInterval(pollCambioEstado, pollingIntervalCambioEstado);
-  });
-});
 
+suscribirAscensor(ip, port).then((idAscensor) => {
+  setInterval(() => { pollAscensor(ip, port, idAscensor) }, pollingIntervalAscensor);
+});
+suscribirCambioEstado(ip, port).then((idCambio) => {
+  setInterval(() => { pollCambioEstado(ip, port, idCambio) }, pollingIntervalCambioEstado);
+});
 
 //alguna interfaz que muestre que esta inicilizando el cliente web
