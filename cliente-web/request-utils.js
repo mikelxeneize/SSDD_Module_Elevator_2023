b2f0a5be26ab261db952ade6e60ead3c6934a48c
Tobias <>
2023-11-01 21:35:56 -0300

    todas las request documentadas

--- cliente-web/request-utils.js
+++ cliente-web/request-utils.js
@@ -1,35 +1,36 @@
 export function sendHttpRequest(ip, port, path, tipo, body) {
-  
-  const url = `http://${ip}:${port}${path}`;
+  return new Promise((resolve, reject) => {
+    const url = `https://${ip}:${port}${path}`;
 
-  const encoder = new TextEncoder();
-  const contenidoEnBytes = encoder.encode(body);
+    const encoder = new TextEncoder();
+    const contenidoEnBytes = encoder.encode(body);
 
-  const options = {
-    method: tipo,
-    headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': contenidoEnBytes.length.toString(),
-    },
-  };
+    const options = {
+      method: tipo,
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': contenidoEnBytes.length.toString(),
+      },
+    };
 
-  if (body !== undefined) {
-    options.body = JSON.stringify(body);
-  }
+    if (body !== undefined) {
+      options.body = JSON.stringify(body);
+    }
 
-  console.log('Request:', url, tipo);
+    console.log('Request:', url, tipo);
 
-  return fetch(url, options)
-    .then((res) => {
-      if (res.ok) {
-        return res.json().then((data) => [res.status, data]);
-      } else {
-        console.log('Error', res.status);
-        return Promise.reject('Error ' + res.status);
-      }
-    })
-    .catch((error) => {
-      console.error('Fetch error:', error);
-      return Promise.reject(error);
-    });
+    return fetch(url, options)
+      .then((res) => {
+        if (res.ok) {
+          return res.json().then((data) => [res.status, data]);
+        } else {
+          console.log('Error', res.status);
+          return Promise.reject('Error ' + res.status);
+        }
+      })
+      .catch((error) => {
+        console.error('Fetch error:', error);
+        return Promise.reject(error);
+      });
+  });
 }
