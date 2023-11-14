dd5e6c6a380d30971ea2b16e37a745a0a6ec8291
Tobias <>
2023-11-14 19:24:36 -0300

    Front ajustes visuales

--- cliente-web/main.js
+++ cliente-web/main.js
@@ -27,19 +27,18 @@ function crearDivAscensor(ascensor) {
     divAscensor.className = "col-lg-4 col-md-6 col-sm-12";
     // Crea el contenido interno del div
     divAscensor.innerHTML = `
-      <div class="card mb-4">
-        <div class="card-header bg-primary text-white">
-          <h5 class="card-title">${ascensor.nombre}</h5>
+        <div class="card mb-4 ">
+            <div class="card-header bg-primary text-white text-center">
+                <h5 class="card-title">${ascensor.nombre}</h5>
+            </div>
+            <div class="card-body">
+                <h6 class="card-subtitle mb-2 text-muted">Estado: ${ascensor.estado}</h6>
+                <h6 class="card-subtitle mb-2 text-muted">Piso Actual: ${ascensor.pisoact}</h6>
+            </div>
+            <div class="card-footer d-flex justify-content-center">
+                <button id="${ascensor.id}-cerrarPuertas" class="btn btn-success">Cerrar Puertas</button>
+            </div>
         </div>
-        <div class="card-body">
-          <h6 class="card-subtitle mb-2 text-muted">Estado: ${ascensor.estado}</h6>
-          <p class="card-text">Piso Actual: ${ascensor.pisoact}</p>
-        </div>
-        <div class="card-footer d-flex justify-content-between">
-          <button id="${ascensor.id}-cerrarPuertas" class="btn btn-success">Cerrar Puertas</button>
-          <button id="${ascensor.id}-detenerAscensor" class="btn btn-danger">Detener Ascensor</button>
-        </div>
-      </div>
     `;
     return divAscensor;
 }
@@ -48,11 +47,6 @@ function crearDivAscensor(ascensor) {
 Event listeners para cada ascensor
 */
 function setupBotones(ascensor) {
-    const botonDetenerAscensor = document.getElementById(`${ascensor.id}-detenerAscensor`);
-    botonDetenerAscensor.addEventListener("click", function () {
-        alert("A quien vas a parar vo' anda palla");
-    });
-
     const botonCerrarPuertas = document.getElementById(`${ascensor.id}-cerrarPuertas`);
     botonCerrarPuertas.addEventListener("click", function () {
         let nuevoEstado = null;
@@ -158,18 +152,3 @@ sendHttpRequest(brokerIp, brokerPort, pathSuscribirCambioEstado, 'POST')
     .catch((error) => {
         console.error('Error en la solicitud cambioEstado:', error);
     });
-
-
-/*
-setInterval(() => {
-const jsonDummy = `[
-    {
-        "idAscensor": "1",
-        "estado": "disponible",
-        "piso": 2,
-        "pisoNuevo": 2
-    }
-]`
-handlePollCambioEstado(JSON.parse(jsonDummy));
-}
-, pollingIntervalCambioEstado * 1.5); */
